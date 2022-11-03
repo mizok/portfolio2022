801023917268f6b164a42d7a9393b1e948d722dc
mizok <karasunohina@gmail.com>
2022-11-04 00:00:32 +0800

    update

--- src/components/layout/layout.module.ts
+++ src/components/layout/layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FooterComponent } from './footer/footer.component';
 import { LayoutComponent } from './layout.component';
 
 @NgModule({
   declarations: [
     SideMenuComponent,
     LayoutComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
